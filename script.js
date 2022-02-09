47977ce66f7f12dcadaa18634068331009369864
Olexiy Konashkov <konashkovolexiy@gmail.com>
2022-02-09 18:13:45 +0200

    v.1

---
+++ script.js
@@ -0,0 +1,14 @@
+let me= {
+    name: "Alexey",
+    age: 14,
+    lang:['JS','HTML','CSS','C#','C++'],
+    address:{
+        street: 'Khorolska',
+        building: '1a',
+        flat: 5,
+    },
+    f:function(text){
+        console.log(text)
+    },
+}
+console.log(me.name)
